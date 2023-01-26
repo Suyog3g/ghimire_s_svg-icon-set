cee43abcdd2632ad835a5cbfc8962c5ea52d6382
Suyog3g <113542889+Suyog3g@users.noreply.github.com>
2023-01-25 23:56:49 -0500

    js updated
    js updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 console.log('JavaScript is running!');
+
+/* longer JS block comment 
+- const is a type of variable - a bit of memory that can hold a value
+
+JS uses (can use) ANY valid CSS selector to make a connection to ANY elment on the page - it works the same way as CSS selectors do, because it's USING css selectors to find matching element(s).
+
+the document is the DOM - the web page and all of its elements
+
+querySelector is the method (function) that makes the connection between JS and the DOM (the elements)
+*/
+
+// step 1 - make the connection to the element(s) you want to interact with
+const facebook = document.querySelector('#facebook');
+const twitter = document.querySelector('#twitter');
+const linkden = document.querySelector('#linkden');
+const pintrest= document.querySelector('#pinterest');
+const skype = document.querySelector('#skype');
+const amazon = document.querySelector('#amazon');
+const soundcloud = document.querySelector('#soundcloud');
+const youtube = document.querySelector('#youtube');
+
+
+function logThisId() {
+    console.log('clicked on this element:', this.id);
+}
+
+// step 2 - decide how you want the user to interact with the obect you created in step 1
+facebook.addEventListener('click', logThisId);
+twitter.addEventListener('click', logThisId);
+linkden.addEventListener('click', logThisId);
+pintrest.addEventListener('click', logThisId);
+skype.addEventListener('click', logThisId);
+amazon.addEventListener('click', logThisId);
+soundcloud.addEventListener('click', logThisId);
+youtube.addEventListener('click', logThisId);
